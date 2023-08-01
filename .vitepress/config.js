d937b8d6ede9335e15687316fb578eeeb4f32641
zcj <18137693952@163.com>
2023-08-01 21:41:39 +0800

    feat: 配置更新

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,28 +1,52 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "leafletjs-example",
-  description: "leafletjs 与 vue3 结合使用的一些示例",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+export default defineConfig(({ mode }) => {
+  const basePath =
+    mode === 'development' ? '/' : '/leafletjs-example.vercel.app/';
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+  return {
+    base: basePath,
+    lang: 'en-ZH',
+    title: 'leafletjs-example',
+    description: 'leafletjs 与 vue3 结合使用的一些示例',
+    themeConfig: {
+      // https://vitepress.dev/reference/default-theme-config
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '示例', activeMatch: `^/examples/`, link: '/examples/' },
+        { text: '贡献者', link: '/team' }
+      ],
+
+      sidebar: {
+        '/examples/': [
+          {
+            text: '快速开始',
+            items: [
+              {
+                text: '简介',
+                link: '/examples/'
+              }
+            ]
+          },
+          {
+            text: '基础示例',
+            items: [
+              {
+                text: '初始化地图',
+                link: '/examples/initMap/'
+              }
+            ]
+          }
         ]
+      },
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/vaebe/leafletjsExample' }
+      ],
+      footer: {
+        message: 'Released under the MIT License.',
+        copyright: 'Copyright © 2023-present vaebe'
       }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vaebe/leafletjsExample' }
-    ]
-  }
-})
+    }
+  };
+});
---
+++ examples/initMap/index.md
@@ -0,0 +1,8 @@
+<script setup>
+import demo from './index.vue'
+</script>
+
+# 使用 leafletjs 结合 vue3 初始化地图
+
+<demo></demo>
+
