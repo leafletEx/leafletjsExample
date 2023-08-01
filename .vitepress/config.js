b8602b06c0bef7d3133f0ef9b26ebe66c3204da3
zcj <18137693952@163.com>
2023-08-01 22:40:44 +0800

    feat: 更新配置

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,30 +1,28 @@
-import { defineConfig } from 'vitepress';
+import { defineConfigWithTheme } from 'vitepress';
 import { i18n } from './config/i18n';
 import { nav } from './config/nav';
 import { sidebar } from './config/sidebar';
 
-// https://vitepress.dev/reference/site-config
-export default defineConfig(({ mode }) => {
-  const basePath =
-    mode === 'development' ? '/' : '/';
-
-  return {
-    base: basePath,
-    lang: 'zh-CN',
-    title: 'leafletjs-example',
-    description: 'leafletjs 与 vue3 结合使用的一些示例',
-    themeConfig: {
-      // https://vitepress.dev/reference/default-theme-config
-      nav,
-      sidebar,
-      i18n,
-      socialLinks: [
-        { icon: 'github', link: 'https://github.com/vaebe/leafletjsExample' }
-      ],
-      footer: {
-        message: 'Released under the MIT License.',
-        copyright: 'Copyright © 2023-present vaebe'
-      }
+export default defineConfigWithTheme({
+  base: '/',
+  lang: 'zh-CN',
+  title: 'leafletjs-example',
+  description: 'leafletjs 与 vue3 结合使用的一些示例',
+  srcDir: 'src',
+  themeConfig: {
+    nav,
+    sidebar,
+    i18n,
+    editLink: {
+      repo: 'https://github.com/vaebe/leafletjsExample/:path',
+      text: '在 GitHub 上编辑此页'
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vaebe/leafletjsExample' }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present vaebe'
     }
-  };
+  }
 });
