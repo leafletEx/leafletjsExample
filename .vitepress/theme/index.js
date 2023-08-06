c05f798f2b1519c28446f5c41eae5cc4a0647afe
zcj <18137693952@163.com>
2023-08-06 13:53:15 +0800

    fix: 打包错误处理

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,17 +1,19 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
-import './customStyle.scss'
+import { h } from 'vue';
+import Theme from 'vitepress/theme';
+import './style.css';
+import './customStyle.scss';
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    });
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
+  async enhanceApp({ app }) {
+    if (typeof window !== 'undefined') {
+      await import('leaflet');
+    }
   }
-}
+};
