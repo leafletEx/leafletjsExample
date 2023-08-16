c9f315229ff2473587c90dc95fb5c694d617c72b
zcj <18137693952@163.com>
2023-08-16 09:49:28 +0800

    fix: 修复因为引入 leaflet-hotline 导致的打包错误

--- src/examples/gradientPolyline/index.vue
+++ src/examples/gradientPolyline/index.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent, onMounted } from 'vue';
 import leafletHotline from 'leaflet-hotline';
-
-// 注册插件
-leafletHotline(L);
 
 const InitMap = defineAsyncComponent(() =>
   import('../../components/InitMapTianditu.vue')
 );
+
+onMounted(() => {
+  // 注册插件 todo 项目使用无需在 onMounted 中调用，这里是为了 vitePress 打包
+  leafletHotline(L);
+});
 
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
