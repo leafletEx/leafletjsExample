b7888e7c6d6857ecdccaa331e0d34dfffd768952
vaebe <18137693952@163.com>
2023-08-19 13:41:50 +0800

    feat: 国际化配置更新、增加本地检索

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -5,6 +5,7 @@ import { MarkdownTransform } from './plugins/markdownTransform.js';
 
 export default defineConfig({
   base: '/',
+  label: '简体中文',
   lang: 'zh-CN',
   title: 'leafletjs-example',
   description: 'leafletjs 与 vue3 结合使用的一些示例',
@@ -58,6 +59,10 @@ export default defineConfig({
     nav,
     sidebar,
     logo: '/logo.png',
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
     outlineTitle: '快速前往',
     editLink: {
       pattern: 'https://github.com/vaebe/leafletjsExample/blob/main/src/:path',
@@ -72,6 +77,29 @@ export default defineConfig({
     footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2023-present vaebe'
+    },
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换'
+                }
+              }
+            }
+          }
+        }
+      }
     }
   },
   vite: {
